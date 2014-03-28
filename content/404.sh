63ad80469b8997f9b681d2a9134c2851a6d38883
Alex Leferry 2 <alexherbo2@gmail.com>
2014-03-28 14:08:45 +0100

    Shell script handling → Markdown

---
+++ content/404.sh
@@ -0,0 +1,3 @@
+replace 'alexherbo2’s user site' 'Page not found'                              \
+         Stormtroopocat           Octobiwan                                    \
+         stormtroopocat           octobiwan                                    < README.md
